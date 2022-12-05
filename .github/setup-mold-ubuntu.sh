47b1df633c8fa531cf562ccc43fd06dd4ee81e7f
Andrew Kvapil <git@viluon.me>
2022-12-05 12:41:34 +0100

    Fix environment variable

--- .github/setup-mold-ubuntu.sh
+++ .github/setup-mold-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 apt-get install -y wget build-essential libgtk-3-dev libasound2-dev libfontconfig1-dev
 ARCH=$(uname -m)
 wget https://github.com/rui314/mold/releases/download/v1.7.1/mold-1.7.1-${ARCH}-linux.tar.gz
