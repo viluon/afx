f2d89265e579ca328d81b55789ffa3a3992b6916
Andrew Kvapil <git@viluon.me>
2022-12-05 12:48:41 +0100

    Install clang

--- .github/setup-mold-ubuntu.sh
+++ .github/setup-mold-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y wget build-essential libgtk-3-dev libasound2-dev libfontconfig1-dev
+apt-get install -y wget clang build-essential libgtk-3-dev libasound2-dev libfontconfig1-dev
 ARCH=$(uname -m)
 wget https://github.com/rui314/mold/releases/download/v1.7.1/mold-1.7.1-${ARCH}-linux.tar.gz
 tar xzf mold-1.7.1-${ARCH}-linux.tar.gz
