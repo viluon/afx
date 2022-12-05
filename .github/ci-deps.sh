7873ca632739a1acad23544b1767cf28e9e9fc15
Andrew Kvapil <git@viluon.me>
2022-12-05 17:00:38 +0100

    Setup release CI via cross-rs (#1)
    * Fix build dependencies
    
    * Fix run condition
    
    * Install mold properly
    
    * Move pre-build configuration to the proper file
    
    🤦
    
    * Fix CI issues
    
    * Improve caching
    
    * Fix script path
    
    * Fix cross pre-build hook
    
    * Install all dependencies via a script
    
    * Avoid sudo
    
    * Fix environment variable
    
    * Install clang
    
    * Use CROSS_DEB_ARCH when pulling dependencies
    
    * Fix CI dependencies
    
    * Rename CI script
    
    * Fix ARM dependencies
    
    * Update upload-artifact
    
    * Only release from pushes to main

---
+++ .github/ci-deps.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+# install dependencies
+dpkg --add-architecture $CROSS_DEB_ARCH
+apt-get update
+apt-get install -y wget clang build-essential libgtk-3-dev:$CROSS_DEB_ARCH libasound2-dev:$CROSS_DEB_ARCH libfontconfig1-dev:$CROSS_DEB_ARCH
+
+# install mold
+ARCH=$(uname -m)
+wget https://github.com/rui314/mold/releases/download/v1.7.1/mold-1.7.1-${ARCH}-linux.tar.gz
+tar xzf mold-1.7.1-${ARCH}-linux.tar.gz
+ls -lhFA mold-1.7.1-${ARCH}-linux
+cp mold-1.7.1-${ARCH}-linux/bin/mold /usr/bin
--- .github/pre-build-release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-apk add --no-cache fontconfig-dev gtk+3.0-dev alsa-lib-dev
