3e6e3b35d39a5c3801787196700faddb84087d3a
Andrew Kvapil <git@viluon.me>
2022-12-05 12:34:30 +0100

    Avoid sudo

--- .github/setup-mold-ubuntu.sh
+++ .github/setup-mold-ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y wget build-essential libgtk-3-dev libasound2-dev libfontconfig1-dev
+apt-get install -y wget build-essential libgtk-3-dev libasound2-dev libfontconfig1-dev
 ARCH=$(uname -m)
 wget https://github.com/rui314/mold/releases/download/v1.7.1/mold-1.7.1-${ARCH}-linux.tar.gz
 tar xzf mold-1.7.1-${ARCH}-linux.tar.gz
 ls -lhFA mold-1.7.1-${ARCH}-linux
-sudo cp mold-1.7.1-${ARCH}-linux/bin/mold /usr/bin
+cp mold-1.7.1-${ARCH}-linux/bin/mold /usr/bin
