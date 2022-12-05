3f1ea7a74a08de2e5b6b48a7b4bc9a54cfcd2246
Andrew Kvapil <git@viluon.me>
2022-12-05 15:45:40 +0100

    Use CROSS_DEB_ARCH when pulling dependencies

--- .github/setup-mold-ubuntu.sh
+++ .github/setup-mold-ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y wget clang build-essential libgtk-3-dev libasound2-dev libfontconfig1-dev
+dpkg --add-architecture $CROSS_DEB_ARCH
+apt-get install -y wget clang clang:$CROSS_DEB_ARCH build-essential build-essential:$CROSS_DEB_ARCH libgtk-3-dev:$CROSS_DEB_ARCH libasound2-dev:$CROSS_DEB_ARCH libfontconfig1-dev:$CROSS_DEB_ARCH
 ARCH=$(uname -m)
 wget https://github.com/rui314/mold/releases/download/v1.7.1/mold-1.7.1-${ARCH}-linux.tar.gz
 tar xzf mold-1.7.1-${ARCH}-linux.tar.gz
