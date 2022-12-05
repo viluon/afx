a09016d91e5c235de2781c7e9bd5f0b98a086743
Andrew Kvapil <git@viluon.me>
2022-12-05 12:27:09 +0100

    Install all dependencies via a script

--- .github/setup-mold-ubuntu.sh
+++ .github/setup-mold-ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install -y wget build-essential
+DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y wget build-essential libgtk-3-dev libasound2-dev libfontconfig1-dev
 ARCH=$(uname -m)
 wget https://github.com/rui314/mold/releases/download/v1.7.1/mold-1.7.1-${ARCH}-linux.tar.gz
 tar xzf mold-1.7.1-${ARCH}-linux.tar.gz
